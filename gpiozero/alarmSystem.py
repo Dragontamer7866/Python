cac1d3f282bcc53f337a82b5f4557f7eb9557fc7
Dragontamer7866 <tacoboycharles@gmail.com>
2025-08-05 12:13:05 -0500

    Update alarmSystem.py

--- gpiozero/alarmSystem.py
+++ gpiozero/alarmSystem.py
@@ -1,10 +1,9 @@
 from SimpleMFRC522 import SimpleMFRC522
 from gpiozero import MotionSensor
-from termcolor import colored
 from gpiozero import Buzzer
 from gpiozero import LED
 import LCD as lcd
-import colorama
+import keyboard
 import time
 import sys
 
@@ -13,53 +12,71 @@ mot = MotionSensor(17)
 red0 = LED(21)
 red1 = LED(13)
 green0 = LED(5)
+blue0 = LED(27)
 lcd.init(0x27, 1)
 lcd.clear()
 reader = SimpleMFRC522()
 bz = Buzzer(22)
+
 alarm = 0
+
+# ---Time Setup---
+current_timestamp = time.ctime()
     
 if __name__ == '__main__':
     try:
         while(True):
-            mot.wait_for_motion()
-            lcd.clear()
-            print("Motion detected!")
-            lcd.write(0,0, "Motion Detected!")
-            motion = True
-            while alarm != 5:
-                red0.on()
-                bz.on()
-                time.sleep(1)
-                bz.off()
-                red0.off()
-                red1.on()
-                bz.on()
-                time.sleep(1)
-                bz.off()
-                red1.off()
-                alarm = alarm + 1
-            alarm = 0
+                    mot.wait_for_motion()
+                    lcd.clear()
+                    print(f"{current_timestamp}: Motion detected!")
+                    lcd.write(0,0, "Motion Detected!")
+                    motion = True
+                    while alarm != 1:
+                        red0.on()
+                        bz.on()
+                        time.sleep(1)
+                        bz.off()
+                        red0.off()
+                        red1.on()
+                        bz.on()
+                        time.sleep(1)
+                        bz.off()
+                        red1.off()
+                        alarm = alarm + 1
+                    alarm = 0
+                        
+                    mot.wait_for_no_motion()
+                    lcd.clear()
+                    print("No motion")
+                    motion = False
+                    lcd.write(0,0, "No Motion")
+                    """
+                    if id == 45610763277:
+                        print("ID: %s\nText: %s" % (id,text))
+                        lcd.clear()
+                        lcd.write(0,0, "Welcome")
+                        lcd.write(0,1, text)
+                        blue0.on()
+                        bz.on()
+                        time.sleep(1)
+                        bz.off()
+                        time.sleep(9)
+                        blue0.off()
                 
-            mot.wait_for_no_motion()
-            lcd.clear()
-            print("No motion")
-            motion = False
-            lcd.write(0,0, "No Motion")
-            print("Hold a tag near the reader to disarm")
-            id, text = reader.read()
-            if id == [PLACEHOLDER] and text == "Charles Dudley":
-                print("ID: %s\nText: %s" % (id,text))
-                green0.on()
-                bz.on()
-                time.sleep(1)
-                bz.off()
-                time.sleep(9)
-                green0.off()
-            
-            else:
-                print(colored("ALERT: BREAK IN!", 'red', attrs=["Underline"]))
-          
+                    else:
+                        print(f"{formatted_datetime}: Suspicious actvity detected. Invalid card used")
+                        lcd.clear()
+                        lcd.write(0,0, "Invaild card") 
+                        lcd.write(0,1, "detected")
+                        bz.on()
+                        red0.on()
+                        red1.on()
+                        time.sleep(10)
+                        red0.off()
+                        red1.off()
+                        bz.off()
+                    """
+      
     except KeyboardInterrupt:
         GPIO.cleanup()
         destroy() 
